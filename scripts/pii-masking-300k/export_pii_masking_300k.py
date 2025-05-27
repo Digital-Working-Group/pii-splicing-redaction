ec557c44371d5b20f539e63973b7b4edf894fabe
ianwong-bu <141453794+ianwong-bu@users.noreply.github.com>
2025-05-27 09:52:51 -0700

    Fix pii masking export dataset loading

--- scripts/pii-masking-300k/export_pii_masking_300k.py
+++ scripts/pii-masking-300k/export_pii_masking_300k.py
@@ -1,6 +1,6 @@
 import datasets
 
-dataset = datasets.load_dataset("json", data_files="/files/hf_datasets/pii-masking-300k/data/train/1english_openpii_30k.jsonl", split="train[:500]")
+dataset = datasets.load_dataset("ai4privacy/pii-masking-300k", data_files="data/train/1english_openpii_30k.jsonl", split="train[:500]")
 
 for i, row in enumerate(dataset):
     with open(str(i) + ".txt", "w") as out_file:
